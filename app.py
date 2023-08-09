622499de887887d8ba277f0629e9073e8ccefc3e
ackrima <roro6125@kakao.com>
2023-08-10 01:51:32 +0900

    login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash
+import re
 
 client = MongoClient('mongodb+srv://sparta:test@cluster0.a0mnxnt.mongodb.net/?retryWrites=true&w=majority')
 
@@ -12,15 +13,21 @@ app.secret_key = 'any random string'
 
 # 홈화면
 @app.route('/')
-def home():
-    id= session.get('id',None)
-    return render_template('index.html',id=id)
+def index():
+    if 'email' in session:
+        email = session['email']
+        logged_in = True
+        return render_template('index.html', email=email, logged_in=logged_in)
+    else:
+        return redirect(url_for('login'))
+
 
 # 전체글 목록조회(list) 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
     all_buckets = list(db.bucket.find({}, {'_id': False}))
     return jsonify({'result': all_buckets})
+
 
 # 좋아요, 싫어요 카운팅-미완성
 @app.route('/update', methods=['POST'])
@@ -34,8 +41,6 @@ def update_like_dislike():
     db.bucket.update_one({'_id': ObjectId(item_id)}, {'$set': {'likeCount': like_count, 'dislikeCount': dislike_count}})
 
     return jsonify({'msg': '업데이트 완료!'})
-
-
 
 
 # 내 방명록 쓰기 및 보기페이지
@@ -63,12 +68,49 @@ def bucket_post():
     return jsonify({'msg': '저장 완료!'})
 
 
+#### 회원가입 로그인 로그아웃
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+users_collection = db['users']
 
 # 회원가입
-@app.route('/join')
-def join():
-    #코드기입
+@app.route('/join', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        existing_user = users_collection.find_one({'email': email})
+
+        if not isValidEmail(email):
+            flash('올바른 이메일 형식이 아닙니다')
+            return render_template('join.html')
+        
+        if not isValidPassword(password):
+            flash('올바른 암호 형식이 아닙니다.')
+            return render_template('join.html')
+
+        if existing_user:
+            flash('이미 사용중인 아이디 입니다')
+            return render_template('join.html')
+
+        new_user = {'email': email, 'password': password}
+        users_collection.insert_one(new_user)
+
+        flash('회원가입성공! 로그인해주세요')
+        return redirect(url_for('login'))
+
     return render_template('join.html')
+
+def isValidEmail(email):
+    emailRegex = re.compile(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')
+    return emailRegex.match(email)
+
+def isValidPassword(password):
+    passwordRegex = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$')
+    return passwordRegex.match(password)
 
 
 
@@ -76,17 +118,26 @@ def join():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        session['id'] = request.form['id']
-        id= session.get('id',None)
-        return render_template('index.html',id=id)
+        email = request.form['email']
+        password = request.form['password']
+
+        user = users_collection.find_one({'email': email})
+
+        if user and user['password'] == password:
+            session['email'] = email
+            return redirect(url_for('index'))
+        else:
+            flash('아이디 또는 비밀번호가 틀렸습니다')
+            return redirect(url_for('login'))
+
     return render_template('login.html')
 
 
-# 로그아웃 - 코드 수정 안해도 됨
+# 로그아웃
 @app.route('/logout', methods=["GET"])
 def logout():
-    session.pop('id', None)
-    return render_template('index.html')
+    session.pop('email', None)
+    return render_template('login.html')
 
 
 
